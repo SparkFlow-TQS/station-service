accb19392a89ff2e2dd82f30a3005cf16e963196
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-05-21 15:14:39 +0100

    fix style issues

--- src/main/java/tqs/sparkflow/station_service/StationServiceApplication.java
+++ src/main/java/tqs/sparkflow/station_service/StationServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class StationServiceApplication {
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 		SpringApplication.run(StationServiceApplication.class, args);
 	}
 }
