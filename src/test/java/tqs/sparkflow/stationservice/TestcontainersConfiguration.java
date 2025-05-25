e4f29aef252a67ff01a45dc331a111d06cb7c38e
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-05-25 16:54:34 +0100

    change test security config

--- src/test/java/tqs/sparkflow/stationservice/TestcontainersConfiguration.java
+++ src/test/java/tqs/sparkflow/stationservice/TestcontainersConfiguration.java
@@ -20,5 +20,8 @@ public class TestcontainersConfiguration {
     registry.add("spring.datasource.url", mysql::getJdbcUrl);
     registry.add("spring.datasource.username", mysql::getUsername);
     registry.add("spring.datasource.password", mysql::getPassword);
+    registry.add("spring.datasource.driver-class-name", () -> "com.mysql.cj.jdbc.Driver");
+    registry.add("spring.jpa.properties.hibernate.dialect", () -> "org.hibernate.dialect.MySQLDialect");
+    registry.add("spring.flyway.enabled", () -> "true");
   }
 }
--- src/test/java/tqs/sparkflow/stationservice/cucumber/CucumberTestConfig.java
+++ src/test/java/tqs/sparkflow/stationservice/cucumber/CucumberTestConfig.java
@@ -38,7 +38,8 @@ public class CucumberTestConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
     return http.build();
   }
 
