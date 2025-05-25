cb749231e9c7920811291c1c36a3b36cdca84071
Gabriel Silva <gabrielmsilva4@ua.pt>
2025-05-25 17:05:21 +0100

    disable flyway in test

--- src/test/java/tqs/sparkflow/stationservice/cucumber/CucumberTestConfig.java
+++ src/test/java/tqs/sparkflow/stationservice/cucumber/CucumberTestConfig.java
@@ -38,7 +38,9 @@ public class CucumberTestConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(csrf -> csrf.disable())
+    http
+        .csrf(csrf -> csrf.disable())
+        .headers(headers -> headers.disable())
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
     return http.build();
   }
