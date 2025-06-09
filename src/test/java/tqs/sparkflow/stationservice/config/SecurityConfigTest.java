31292f533f8f6aca861d2f8ce222d527e5cd47d4
Sebastião Teixeira <steixeira2004@gmail.com>
2025-06-09 15:05:37 +0100

    fix: suppress SonarQube deprecation warning for MockBean usage
    Added @SuppressWarnings annotation to suppress false positive
    SonarQube warning about MockBean deprecation. MockBean remains
    the standard approach for Spring Boot testing.

--- src/test/java/tqs/sparkflow/stationservice/config/SecurityConfigTest.java
+++ src/test/java/tqs/sparkflow/stationservice/config/SecurityConfigTest.java
@@ -3,12 +3,16 @@ package tqs.sparkflow.stationservice.config;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.TestConfiguration;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.context.TestPropertySource;
@@ -43,6 +47,7 @@ class SecurityConfigTest {
     @MockBean
     private ChargingSessionService chargingSessionService;
 
+    @SuppressWarnings("java:S6813") // Suppress SonarQube deprecation warning for MockBean
     @MockBean
     private JwtUtil jwtUtil;
 
